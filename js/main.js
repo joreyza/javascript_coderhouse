6f264f250257392efcfb41b4b2d6a50a7d8c99ec
Jorge Reyes <joreyza@gmail.com>
2021-08-08 11:40:07 -0400

    agregando funciones

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-prompt("Por favor ingrese el monto en Pesos chilenos (CLP)");
-prompt("Por favor ingrese la moneda de destino: las opciones son "USD" y "EUR"");
+var montoOriginal = parseInt(prompt("Por favor ingrese el monto en Pesos chilenos (CLP)"));
+var monedaFinal = prompt("Por favor ingrese la moneda de destino: las opciones son 'USD' y 'EUR'");
+
+const convertirEUR = function (cantidad){
+    return cantidad*0.0011;
+} 
+
+const convertirUSD = function (cantidad){
+    return cantidad*0.0013;
+}
 
 
-const 
+let resultado = convertirEUR(montoOriginal);
+let resultado2 = convertirUSD(montoOriginal);
+
+
+
+alert("el monto es: "+ resultado);
+
+alert("el monto es: "+ resultado2);
